015418808c9b2912b3935c1d386a04ae0ba99be1
Jenna-Smith <jennawsmith@outlook.com>
2017-06-09 11:37:52 -0400

    add presidents var array with objects

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var presidents = [
+    {firstName: 'Donald', lastName: 'Trump', termLength: null, party: 'Republican', yearsOfPresidency: null},
+
+    {firstName: 'Barrack', lastName: 'Obama', termLength: 2, party: 'Democratic', yearsOfPresidency: 8},
+
+    {firstName: 'George W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8},
+
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democratic', yearsOfPresidency: 8},
+
+    {firstName: 'George H.W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8}
+];
+
+console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
