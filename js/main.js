99ab150c69dc66b897ff25f4c9b72c15ff872992
Jenna-Smith <jennawsmith@outlook.com>
2017-06-09 12:12:37 -0400

    adjust years of presidency and terms

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 var presidents = [
-    {firstName: 'Donald', lastName: 'Trump', termLength: null, party: 'Republican', yearsOfPresidency: null},
+    {firstName: 'Donald', lastName: 'Trump', termLength: null, party: 'Republican', yearsOfPresidency: '2017-present'},
 
-    {firstName: 'Barrack', lastName: 'Obama', termLength: 2, party: 'Democratic', yearsOfPresidency: 8},
+    {firstName: 'Barrack', lastName: 'Obama', termLength: 2, party: 'Democratic', yearsOfPresidency: '2009-2017'},
 
-    {firstName: 'George W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8},
+    {firstName: 'George W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: '2001-2009'},
 
-    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democratic', yearsOfPresidency: 8},
+    {firstName: 'Bill', lastName: 'Clinton', termLength: 2, party: 'Democratic', yearsOfPresidency: '1993-2001'},
 
-    {firstName: 'George H.W.', lastName: 'Bush', termLength: 2, party: 'Republican', yearsOfPresidency: 8}
+    {firstName: 'George H.W.', lastName: 'Bush', termLength: 1, party: 'Republican', yearsOfPresidency: '1989-1993'}
 ];
 
 console.log(presidents[2].firstName + ' ' + presidents[2].lastName);
