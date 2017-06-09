3b36a55f68f85f80d609c0109cc6a34e88d02dbb
Jenna-Smith <jennawsmith@outlook.com>
2017-06-09 12:28:49 -0400

    add numba var with function

--- js/main.js
+++ js/main.js
@@ -3,3 +3,19 @@ function howdy() {
 }
 
 howdy();
+
+var numba = function (string) {
+    if (string.length < 7) {
+        console.log('What a short little word!');
+    }
+    if (string.length > 7) {
+        console.log("I'm sorry, but that's too many to count.");
+    }
+    if (string.length === 7) {
+        console.log('7, what a perfect choice!');
+    }
+};
+
+console.log(numba('7'));
+console.log(numba('seventy'));
+console.log(numba('university'));
